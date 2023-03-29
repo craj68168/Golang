92565b7415a39acb6328b57371a876ae16ce9667
Raj Chaudhary <craj68168@gmail.com>
2023-03-29 16:19:35 +0545

    feat:variables and its types

---
+++ 02variables/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+const LoginToken string = "mytoken" // capital L means we make it public and it constant and use it inside method as it is globally declare
+func main() {
+	var username string = "Raj"
+	fmt.Println(username)
+	fmt.Printf("Variable username is type: %T \n", username)
+
+	var isBool bool = false
+	fmt.Println(isBool)
+	fmt.Printf("Variable isBool is type: %T \n", isBool)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable smallVal is type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.554433
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable smallFloat is type: %T \n", smallFloat) //it gives 255.55443 only
+
+	var smallFloat2 float64 = 255.554433221100
+	fmt.Println(smallFloat2)
+	fmt.Printf("Variable smallFloat2 is type: %T \n", smallFloat2) //it gives 255.5544332211 only
+
+	// default values and some aliases
+	var defaultValInt int
+	fmt.Println(defaultValInt)                                         // default value will be 0
+	fmt.Printf("Variable defaultValInt is type: %T \n", defaultValInt) // int
+
+	var defaultValString string
+	fmt.Println(defaultValString)                                            //  nothing
+	fmt.Printf("Variable defaultValString is type: %T \n", defaultValString) // string
+
+	// implicit way of declaring variables
+	var myString = "RaZz"
+	fmt.Println(myString)                                    //  print it
+	fmt.Printf("Variable myString is type: %T \n", myString) // decalre to string
+	// cannot do myString = 3
+
+	// no var style
+	myNumber := 123456.90 // inside any method you are allowed to use this walrus operator not outside of any method.
+	fmt.Println("number", myNumber)
+
+	fmt.Println("LoginToken", LoginToken)
+}
