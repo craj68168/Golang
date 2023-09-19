2bfea70a28857916f613bf5c65dfc820f3200294
Raj Chaudhary <craj68168@gmail.com>
2023-09-19 22:21:42 +0545

    feat:conversion in golang

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -4,18 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
 	fullName := bufio.NewReader(os.Stdin)
-	fmt.Println("Please Enter Your Name:")
+	fmt.Println("Please Enter Your Rating:")
 
-	input, err := fullName.ReadString('\n')
+	input, err := fullName.ReadString('\n') // \n means hitting enter
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Your full name is", input)
+	fmt.Println("Your rating is", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64) // parse string to flat
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("num rating", numRating+1)
 }
